06c1cb6083fd299eb44b644856ec17f978811009
luozhenjie1997 <1356913256@qq.com>
2024-10-04 17:38:49 +0800

    Scripts for installing CPL-Diff.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='CPLDiff',
+    packages=find_packages(),
+    version='1.0.0',
+    description='asd',
+    author='Zhenjie Luo',
+    python_requires='>=3.8',
+)
